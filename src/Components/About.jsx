383da27006247167546ddebd41fc82fd945fa4f0
PROGRAMMER-AK003 <157791752+PROGRAMMER-AK003@users.noreply.github.com>
2024-05-23 10:10:37 +0000

    update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Hi there I am Aditya a Hardworking and enthusiastic College Student.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -61,7 +61,7 @@ const Footer = (props) => {
         }}
       >
         {email && (
-          <a href={`mailto:${email}`}>
+          <a href={`mailto:adityakanojiya003@gmail.com`}>
             <img src={envelopeIcon} alt="email" className="socialIcon" />
           </a>
         )}
@@ -71,7 +71,7 @@ const Footer = (props) => {
           </a>
         )}
         {gitHub && (
-          <a href={`https://github.com/${gitHub}`} target="_blank" rel="noopener noreferrer">
+          <a href={`https://github.com/PROGRAMMER-AK003`} target="_blank" rel="noopener noreferrer">
             <img src={gitHubIcon} alt="GitHub" className="socialIcon" />
           </a>
         )}
@@ -86,7 +86,7 @@ const Footer = (props) => {
         )}
         {linkedIn && (
           <a
-            href={`https://www.linkedin.com/in/${linkedIn}`}
+            href={`https://www.linkedin.com/in/aditya-kanojiya-5882412b0/`}
             target="_blank"
             rel="noopener noreferrer"
           >
